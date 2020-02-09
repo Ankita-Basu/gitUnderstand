dc2aeea1be62551c6e595ad8ae8c97075bc73c3d
Ankita-Basu <ankitabasu30@gmail.com>
2020-02-09 23:18:55 +0530

    add the new changes

---
+++ index.js
@@ -0,0 +1,32 @@
+//scrippt k andr jo b hota h,
+        // that needs to talk to the elements of webpage
+        //third party codes should be in head section
+        //statement is piece of code which tells the action to be carried-out
+        //following is logging a statement to console 
+        console.log('hello');
+        let name = 'basu' ;
+        console.log(name);
+         //rules of naming variables : no reserved keywords, should be meaning  full,
+         // cant start with number, conot contain space or hyphen, case sensitive,
+         //multiple varibles can be declared (modern best pracctice is to declare veriables in same line)
+         //initial value can be changed later
+         let interestRate=0.3;
+         nterestRate = 1;
+         console.log(nterestRate);
+         //in real world its not recomended to change the variable value
+         //orelse will cause problem
+         //cause bugs
+         /*to keep a value non changeable  we use constant-keyword: const in place of let */ 
+         /*const interestRate=0.3;*/
+         nterestRate = 1;
+         console.log(nterestRate);
+         //error will come as  we cant reassign a constant
+         //constant should be a default choise if we donot want to reassign
+         let age = 25;
+         let isGirl = true;
+         //can expricitly say undefinded or else by default it is undefined
+         let firstName = undefined;
+         //null is used to expricitly clear the value of variable
+         //let selectedColor = null ; so here if the use wants the color to be ,say blue developer can  make it blue
+         let selectedColor = null;
+          
